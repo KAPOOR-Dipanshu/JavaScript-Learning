e1b872362fe3646322eab6e9de4df637b484dabc
DIPANSHU-KAPOOR <71254521+KAPOOR-Dipanshu@users.noreply.github.com>
2023-10-14 16:49:39 +0000

    String to No

---
+++ dataTypeConversion/conversion.js
@@ -0,0 +1,16 @@
+let score = "69"
+let score_2 = "33abc"
+
+// console.log(score,typeof(score))
+// console.log(score_2,typeof(score_2))
+
+let valueInNumber = Number(score)
+let valueInNumber_2 = Number(score_2)
+
+// console.log(valueInNumber,typeof(valueInNumber))
+// console.log(valueInNumber_2,typeof(valueInNumber_2))
+
+let phnNumber = 8376991103
+
+let noPhone = String(phnNumber)
+console.log(noPhone,typeof(noPhone));
