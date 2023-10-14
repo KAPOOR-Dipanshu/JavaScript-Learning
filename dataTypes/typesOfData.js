ec5ee1aaabcd06e883f07383b73624b13f2a8b99
DIPANSHU-KAPOOR <71254521+KAPOOR-Dipanshu@users.noreply.github.com>
2023-10-14 09:20:42 +0000

    typeOf and null and undefined

--- dataTypes/typesOfData.js
+++ dataTypes/typesOfData.js
@@ -1,9 +1,9 @@
 const accountId = 123454321
 let emailId = 'dipanshu@gmail.com'
 var bankBalance = 45673
-cityLiving = 'Jaipur'
-let name;
-
+let cityLiving; 
+let name = null
 // accountId = 342
 
 console.table([accountId,emailId,bankBalance,cityLiving,name])
+console.table([typeof(accountId),typeof(emailId),typeof(bankBalance),typeof(cityLiving),typeof(name)])
