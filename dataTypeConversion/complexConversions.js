dc6336477e3872f2b273e824c3f3a85a1a6c6bcd
DIPANSHU-KAPOOR <71254521+KAPOOR-Dipanshu@users.noreply.github.com>
2023-10-14 17:54:11 +0000

    pre & post increement

---
+++ dataTypeConversion/complexConversions.js
@@ -0,0 +1,11 @@
+console.log("1" + 2 + "3"); 
+console.log(1 + 2 + "3");
+console.log(1 + 5 + "2" + 3);
+// it converts all the inputs based on the first one
+console.log(9 + true); // the value doesnot change but typecasted to integer
+console.log( 2 * ""); // the value of empty string is false but typecasted to integer
+
+let x = 5
+let y = ++x
+console.log(++x);
+console.log(y);
