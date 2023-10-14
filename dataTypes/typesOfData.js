d9c39347ccd76eaeb8912dec68614a2d42eb636e
DIPANSHU-KAPOOR <71254521+KAPOOR-Dipanshu@users.noreply.github.com>
2023-10-14 08:51:34 +0000

    Class 1 completed

---
+++ dataTypes/typesOfData.js
@@ -0,0 +1,9 @@
+const accountId = 123454321
+let emailId = 'dipanshu@gmail.com'
+var bankBalance = 45673
+cityLiving = 'Jaipur'
+let name;
+
+// accountId = 342
+
+console.table([accountId,emailId,bankBalance,cityLiving,name])
